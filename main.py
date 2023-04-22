89c7d8e251ef6e06df90580aea004e192a053dfb
mikoS223 <sowamikolaj223@gmail.com>
2023-04-22 12:31:24 +0200

    final sensory

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_excel('protocol3.xlsx')
+df = pd.read_excel('fotoelektryka.xlsx')
 
-x = df["Material"]
-h = df["H"]
+x = df["deg"]
+
 #
 # plt.plot(x, df["off1"], "bx",  x, df["off2"], "bx", x, df["off3"], "bx")
 # plt.plot(x, df["on1"], "rv", x, df["on2"], "rv", x, df["on3"], "rv")
+
+# plt.plot(x, df["off1"], "bx", label="wyłączenie")
+# plt.plot(x, df["on1"], "rv", label="załączenie")
+# plt.legend()
 #
-# plt.xticks(x)
-#
-plt.plot(x, h, "rs")
-plt.xlabel("Numer Materiału")
-plt.ylabel("Histereza [mm]")
+# plt.plot(x, df["off2"], "bx", )
+# plt.plot(x, df["on2"], "rv", )
+# plt.plot(x, df["off3"], "bx", )
+# plt.plot(x, df["on3"], "rv", )
+
+#plt.plot(x, abs(df["mih"]), "s")
+plt.plot(x, df["P"])
+plt.plot(x, df["P"], "s")
+# plt.plot(x, df["I"], label="")
+# plt.plot(x, df["P"], label="")
+
+plt.xticks(x)
+
+#plt.plot(x, h, "rs")
+plt.xlabel("Odchylenie od prostopadłości [°]")
+plt.ylabel("Moc [mW]")
 
 
 plt.show()
